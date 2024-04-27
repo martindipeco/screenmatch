f206923199e76db9c7627b528bc3b6fccf231ef5
martindipeco <martindipeco@gmail.com>
2024-04-27 17:28:24 -0300

    agrego para clase episodio

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ApiKey;
@@ -27,13 +28,16 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		String busqueda = "runner";
 
 		String direccion = "https://www.omdbapi.com/?t=" + busqueda + "&apikey=" + clave;
-
+		String direccionEpisodio = "https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=" + clave;
 		//ConsumoAPI consumoAPI = new ConsumoAPI();
 		var consumoAPI = new ConsumoAPI();
 		var json = consumoAPI.obtenerDatos(direccion);
+		var jsonEpisodio = consumoAPI.obtenerDatos(direccionEpisodio);
 
 		ConvierteDatos conversor = new ConvierteDatos();
 		var datos = conversor.obtenerDatos(json, DatosSerie.class);
+		var datosEpisodio = conversor.obtenerDatos(jsonEpisodio, DatosEpisodio.class);
 		System.out.println(datos);
+		System.out.println(datosEpisodio);
 	}
 }
